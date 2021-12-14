05a1a5e818003745106b51da9230f945e6ce64e6
rhedwan <adesolaridwan2003@gmail.com>
2021-12-14 11:56:00 +0100

    Fixed the addEventListener action

--- Documents/project/index.js
+++ Documents/project/index.js
@@ -1,16 +1,19 @@
 const form = document.querySelector('.form')
 const submitBtn = document.querySelector('.submitBtn');
 const inputTodo = document.querySelector('.inputTodo');
-
-
+const todo = inputTodo.value;
 
 // //adding event listener to form
-submitBtn.addEventListener('submit', addItem);
+submitBtn.addEventListener('click', addItem);
 
 
 // Adding of todos Item
 function addItem(e){
     e.preventDefault();
-  
+    if (!inputTodo.value){
+        alert("We need info!!..")
+        return ;
+    }
+    console.log(todo);
 };
 
